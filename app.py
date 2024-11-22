4ec1243566b2e5bb7ed52d2b2e56a2c959ec0f40
matper78 <mattiaperrone3@gmail.com>
2024-11-22 12:36:12 +0000

    esercizio2

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def welcome():
+    return render_template("welcome.html")
+
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 app.run()
